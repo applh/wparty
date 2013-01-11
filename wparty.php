b98618af38009fdd6f8ac61562412526331e976d
Applh.com <github@applh.com>
2013-01-11 09:34:46 -0800

    Create wparty.php

---
+++ wparty.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WParty
+Plugin URI: http://applh.com/wordpress/plugins/wparty/
+Description: WParty will provide a shortcode [part name="page-name"] to mix pages/articles/media content
+Version: 1.0
+Author: Applh
+Author URI: http://Applh.com
+License: GPLv3
+*/
+
+?>

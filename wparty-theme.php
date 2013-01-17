9bf56fec242123317793309a4bd4094fc84e2f94
Applh.com <github@applh.com>
2013-01-16 16:54:48 -0800

    Create wparty-theme.php

---
+++ wparty-theme.php
@@ -0,0 +1,18 @@
+<?php
+
+function wparty_filter_loop ($res) {
+     ob_start();
+
+     if (have_posts()) {
+        while (have_posts()) { 
+           the_post();
+           echo '<h3>';The_title();echo '</h3>';   
+           echo '<div>';The_content();echo '</div>';   
+        }
+     }
+    $res=ob_get_clean();
+    return $res;
+}
+
+add_filter('wparty_response', 'wparty_filter_loop');
+

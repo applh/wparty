1e26722597eea10564bb49930d52afc8e29b0cde
Applh.com <github@applh.com>
2013-01-22 22:19:17 +0100

    Add command line by shortcode [wparty var="" val=""]

--- wparty-admin.php
+++ wparty-admin.php
@@ -11,7 +11,8 @@ $WParty['admin_html']=
 <tbody>
 <tr>
 <td>
-<textarea rows="20" cols="80">
+<textarea name="wparty_cmd" rows="20" cols="80">
+[wparty var="" val=""]
 </textarea>
 </td>
 </tr>
@@ -23,12 +24,51 @@ $WParty['admin_html']=
 </tbody>
 </table>
 </form>
+<div>
+<!--WPARTY-MSG-->
+</div>
 WPARTYADMIN;
+
+
+function shortcode_wparty ($atts) {
+   $res='';    
+   extract(shortcode_atts( array( 'var' => '', 'val' => ''), $atts));
+   $res.="SET $var = $val";
+
+   global $WParty;
+   if (empty($WParty['admin.cmd.response'])) {
+      $WParty['admin.cmd.response']=$res;
+   }
+   else {
+      $WParty['admin.cmd.response']=$WParty['admin.cmd.response'].$res;
+   }
+   return $res;
+}
 
 function wparty_admin () {
    global $WParty;
-   
-   echo $WParty['admin_html'];
+   $cmd='';
+   if (!empty($_REQUEST['wparty_cmd'])) {
+      $cmd=trim($_REQUEST['wparty_cmd']);
+   }
+   $htmlcmd='';
+   if ($cmd) {
+      add_shortcode( 'wparty', 'shortcode_wparty' );
+
+      $WParty['admin_cmd']=$cmd;
+      do_shortcode(stripslashes($cmd));
+
+      $htmlcmd='<textarea cols="80" rows="20" readonly>'.$WParty['admin.cmd.response'].'</textarea>';
+   }
+
+   $trans=array(
+      "<!--WPARTY-MSG-->" => $htmlcmd,
+   );
+   $html=str_replace(array_keys($trans), 
+                     array_values($trans), 
+                     $WParty['admin_html']);
+
+   echo $html;
 
 }
 
@@ -38,4 +78,5 @@ function wparty_admin_init () {
 
 
 add_action('admin_menu', 'wparty_admin_init');
+
 

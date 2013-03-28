78c2baa2702e9a272c4c9e2cc5284a64dffe190f
Applh.com <github@applh.com>
2013-03-28 15:37:28 +0100

    v1.6.1 + add widget 'list'
    * add widget 'list'
    * add protection against infinite loop (recursive max=10)

--- wparty.php
+++ wparty.php
@@ -3,7 +3,7 @@
 Plugin Name: WParty
 Plugin URI: http://applh.com/wordpress/plugins/wparty/
 Description: WParty will provide a shortcode [part name="page-name"] to mix pages/articles/media/widgets/menus content
-Version: 1.6
+Version: 1.6.1
 Author: Applh
 Author URI: http://Applh.com
 License: GPLv3
@@ -13,9 +13,14 @@ $curdir=dirname(__FILE__);
 
 global $WParty;
 $WParty=array(
-   "version" => "1.6",
+   "version" => "1.6.1",
    "wparty.dir" => $curdir,
 );
+
+global $WPartyRecursive;
+global $WPartyMaxRecursive;
+$WPartyRecursive=0;
+$WPartyMaxRecursive=10;
 
 
 // shortcode [part name="page-name"]
@@ -23,6 +28,15 @@ $WParty=array(
 // shortcode [part menu="my-menu" name="page-name"]
 
 // http://codex.wordpress.org/Template_Tags/get_posts
+
+// shortcode example:
+// [part widget="list" args="numberposts=5&tag=my-tag1,my-tag2"]
+// <a href="PERMALINK">TITLE</a>
+// CONTENT
+// <small>TAGS</small> / <small>CATS</small>
+// <small>DATE</small>
+// [/part]
+
 // shortcode [part widget="loop" args="numberposts=5&tag=my-tag1,my-tag2"]
 
 // http://codex.wordpress.org/Function_Reference/the_widget
@@ -36,21 +50,28 @@ $WParty=array(
 // shortcode [part widget="menu" instance="nav_menu=toto"]
 // shortcode [part widget="slider" name="my-slider"]
 
-// WARNING: RECURSIVE CODE CAN BE DANGEROUS
+// NOTE: RECURSIVE CODE LIMIT TO 10
 // shortcode [part widget="sidebar" name="theme-sidebar-name"]
 
 // THEME BUILDER
 // shortcode [part theme="My Theme" name="new-theme"]
 
-// DEV:
+// DEV
 // shortcode [part name="page-name" start="01-12-2013" end="08-12-2013"]
 // shortcode [part widget="redirect" instance="/url2/"]
 // shortcode [part if="lang=fr" widget="redirect" instance="/url2/"]
 // shortcode [part meta="extra-name"]
 
-
 function shortcode_part ($atts, $content, $tag) {
-    global $WParty;
+
+   // PROTECTION AGAINST INFINITE LOOP
+   global $WPartyRecursive;
+   global $WPartyMaxRecursive;
+   $WPartyRecursive++;
+   if ($WPartyRecursive > $WPartyMaxRecursive) 
+      return '';
+
+   global $WParty;
 
     $res='';
     
@@ -135,7 +156,10 @@ function shortcode_part ($atts, $content, $tag) {
          $widget=strtolower(trim($widget));
          ob_start();
 
-         if ($widget == 'loop') {
+         if ($widget == 'list') {
+       	   wparty_widget_list('', $instance, $args, $content);
+         }
+         else if ($widget == 'loop') {
        	   wparty_widget_loop('', $instance, $args);
          }
          else if ($widget == 'slider') {
@@ -522,7 +546,94 @@ MODEL0;
 //    return $res;
 }
 endif;
-			
+
+if (!function_exists('wparty_widget_list')) :
+function wparty_widget_list ($res, $instance, $args, $content='') {
+   global $WParty;
+
+//     ob_start();
+     $N="\n";
+   $defaults = array(
+	                'numberposts' => 5, 
+                        'post_type' => 'post',
+	                'post_status' => 'publish',
+	        );
+   $tab_args = wp_parse_args( $args, $defaults );   
+   global $post;
+   $myposts = get_posts( $tab_args );
+
+   $model0=
+<<<MODEL0
+<div class="entry">
+ <h3 class="entry-title"><a class="entry-link" href="PERMALINK">TITLE</a></h3>
+ <div class="entry-content">
+CONTENT
+ </div>
+ <hr/>
+ <div class="entry-date">DATE</div>
+ <div class="entry-tags">TAGS</div>
+ <div class="entry-cats">CATS</div>
+</div>
+MODEL0;
+
+   if (!empty($content)) {
+      $model0=$content;
+   }
+
+   $tags2sep=", ";
+   $tags2before="";
+   $tags2after="";
+
+   $date2format="";
+   $date2before="";
+   $date2after="";
+
+   $cats2sep=", ";
+
+   $loop2model="loop.model$instance";
+
+   if (!empty($WParty["$loop2model"])) $model0=$WParty["$loop2model"];
+   if (!empty($WParty['tags.sep'])) $tags2sep=$WParty['tags.sep'];
+   if (!empty($WParty['tags.before'])) $tags2sep=$WParty['tags.before'];
+   if (!empty($WParty['tags.after'])) $tags2sep=$WParty['tags.after'];
+
+   if (!empty($WParty['date.format'])) $date2format=$WParty['date.format'];
+   if (!empty($WParty['date.before'])) $date2before=$WParty['date.before'];
+   if (!empty($WParty['date.after'])) $date2after=$WParty['date.after'];
+
+   if (!empty($WParty['cats.sep'])) $cats2sep=$WParty['cats.sep'];
+
+   if (count($myposts) > 0) {
+      $model0=do_shortcode($model0);
+   }
+
+   foreach( $myposts as $post ) {	
+      setup_postdata($post);
+
+      $tags2html=get_the_tag_list($tags2before, $tags2sep, $tags2after);
+      //$date2html=get_the_date($date2format, $date2before, $date2after, false);
+      $date2html=$date2before.get_the_date($date2format).$date2after;
+      $cats2html=get_the_category_list($cats2sep);
+
+      $translate=array(
+"TITLE" => get_the_title(),
+"PERMALINK" => get_permalink(),
+"CONTENT" => apply_filters('the_content', get_the_content()),
+"TAGS" => $tags2html,
+"CATS" => $cats2html,
+"DATE" => $date2html,
+"WPARTY-MODEL" => $loop2model,
+      );
+
+      $htmlpost=str_replace(array_keys($translate), array_values($translate), $model0);
+      echo $htmlpost;
+   }
+
+//    $res.=ob_get_clean();
+//    return $res;
+}
+endif;
+	
 function wparty ($part, $attr=null) {
 
    global $WParty;

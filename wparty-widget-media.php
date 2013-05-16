dcd697d8c601225eac2b6cf580cad0baa470926b
Applh.com <github@applh.com>
2013-05-16 16:13:55 +0200

    dev ongoing
    dev on widget media

--- wparty-widget-media.php
+++ wparty-widget-media.php
@@ -4,9 +4,15 @@ if (!function_exists('wparty_widget_media')) :
 function wparty_widget_media ($res, $instance, $args, $content='') {
    global $WParty;
    
+   $content=trim($content);
+
+   if (empty($content)) {
+      $content = "/media.png";
+   }
+
    $res0 = "";
 
-   $res1 = '<img src="/media.png" />';
+   $res1 = '<img src="'.$content.'" />';
 
    $res2 = "";
 

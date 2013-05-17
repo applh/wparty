1cefd550408846744f56af6aca6b784f759f8839
Applh.com <github@applh.com>
2013-05-17 18:35:10 +0200

    debug max default value
    widget lorem default max = 500

--- wparty-widget-lorem.php
+++ wparty-widget-lorem.php
@@ -10,8 +10,11 @@ function wparty_widget_lorem ($res, $instance, $args, $content='') {
 
    $res2 = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.";
 
-   $max = intval($WParty['part.max']);
-   
+   if (!empty($WParty['part.max']))
+      $max = intval($WParty['part.max']);
+   else 
+      $max=500;
+
    $res2text = substr("$res0 $res1 $res2", 0, $max);
 
    $res .= $res2text;

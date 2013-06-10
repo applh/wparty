c6e0e83d1418f813afe92472d3e5565fe44a48ee
Applh.com <github@applh.com>
2013-06-10 11:56:27 +0200

    add css to TR and TD

--- wparty-widget-csv.php
+++ wparty-widget-csv.php
@@ -15,18 +15,23 @@ function wparty_widget_csv () {
 
    $tabcsv=explode("\n", $content);
    $htmltable='';
+   $row2count=0;
    foreach($tabcsv as $r => $rowdata) {
       $rowdata=trim($rowdata);
       if ($rowdata) {
          $tabrow=str_getcsv($rowdata, $cut, $quote, $esc);
 
          $htmlrow='';
+         $col2count=0;
          foreach($tabrow as $c => $coldata) {
             $coldata=trim($coldata);
-            $htmlrow.='<td>'.$coldata.'</td>';
+            $htmlrow.='<td class="col'.$col2count.' cell'.$row2count.'x'.$col2count.'">'.$coldata.'</td>';
+             
+            $col2count++;
          }
-         if ($htmlrow) $htmlrow='<tr>'.$htmlrow.'</tr>';
+         if ($htmlrow) $htmlrow='<tr class="row'.$row2count.'">'.$htmlrow.'</tr>';
          $htmltable.=$htmlrow;
+         $row2count++;
       }
    }
 

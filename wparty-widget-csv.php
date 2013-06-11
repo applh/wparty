988a2a5edd153f9646ff6ae358a5bab4e8607c68
Applh.com <github@applh.com>
2013-06-11 11:21:34 +0200

    add <br> to multiline cells
    change code to handle multiline cells

--- wparty-widget-csv.php
+++ wparty-widget-csv.php
@@ -1,11 +1,10 @@
 <?php
 
-if (!function_exists('wparty_widget_csv')) :
-function wparty_widget_csv () {
+if (!function_exists('wparty_widget_csv_file')) :
+function wparty_widget_csv_file () {
    global $WParty;
 
-   $content=trim($WParty['part.csv']);
-   
+   $handle=$WParty['part.handle'];
    $cut=trim($WParty['part.cut']);
    $quote=trim($WParty['part.quote']);
    $esc=trim($WParty['part.esc']);
@@ -14,20 +13,18 @@ function wparty_widget_csv () {
    if (!$quote) $quote='"';
    if (!$esc) $esc='\\';
 
-   $tabcsv=explode("\n", $content);
    $htmltable='';
    $row2count=0;
-   foreach($tabcsv as $r => $rowdata) {
-      $rowdata=trim($rowdata);
-      if ($rowdata) {
-         $tabrow=str_getcsv($rowdata, $cut, $quote, $esc);
-
-         $htmlrow='';
+   //ini_set('auto_detect_line_endings', '1');
+   while (($tabrow = fgetcsv($handle, 0, $cut, $quote, $esc)) !== FALSE) {
+     if (is_array($tabrow)) {
          $col2count=0;
+         $htmlrow='';
          foreach($tabrow as $c => $coldata) {
             $coldata=trim($coldata);
             $htmlrow.='<td class="col'.$col2count.' cell'.$row2count.'x'.$col2count.'">'.$coldata.'</td>';
-             
+            // multiline cells ?
+            $htmlrow=nl2br($htmlrow); 
             $col2count++;
          }
          if ($htmlrow) $htmlrow='<tr class="row'.$row2count.'">'.$htmlrow.'</tr>';
@@ -35,10 +32,30 @@ function wparty_widget_csv () {
          $row2count++;
       }
    }
-
    if ($htmltable) $htmltable='<table><tbody>'.$htmltable.'</tbody></table>';
 
    echo $htmltable;
+}
+endif;
+
+
+if (!function_exists('wparty_widget_csv')) :
+function wparty_widget_csv () {
+   global $WParty;
+
+   $content=trim($WParty['part.csv']);
+   if (($handle = fopen("php://memory", "w+")) !== FALSE) {
+      $WParty['part.handle']=$handle;
+      fwrite($handle, $content);
+      fseek($handle, 0);
+
+      wparty_widget_csv_file();
+
+      $WParty['part.handle']=null;
+      fclose($handle);
+   }
+   
+
 }
 
 endif;
@@ -64,8 +81,5 @@ function wparty_widget_csv2 () {
  }
 
 endif;
-
-
-
 
 

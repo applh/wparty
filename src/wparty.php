dc1b04f8d762192964172d7cd17e93418d233d14
Applh.com <github@applh.com>
2014-03-19 13:13:41 +0100

    move most code in folder src
    Only keep index.php in root folder.
    This will allow basic versioning of code sources.
    index.php defines $WPartyVersion to target the good folder (/src/ by
    default)

--- src/wparty.php
+++ src/wparty.php
@@ -1,16 +1,4 @@
 <?php
-/*
-Plugin Name: WParty
-Plugin URI: http://applh.com/wordpress/plugins/wparty/
-
-Version: 2.0.0
-
-Description: WParty adds a shortcode [part name="page-name"] to easily mix content: pages/articles/media/widgets/menus. Read more... http://wordpress.org/plugins/wparty/
-
-Author: Applh
-Author URI: http://Applh.com
-License: GPLv3
-*/
 
 if (!function_exists('add_shortcode')) die();
 
